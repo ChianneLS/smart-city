c23753ab38a9cc760d4b6d43f7c545ecc76e0b30
Chianne <chiannelyfordshields@gmail.com>
2024-10-31 12:49:26 +1300

    Final changes pre nerdfest showcase

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 function plant_temperature_check () {
-    if (Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P15) < 15) {
+    if (Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P7) < 15) {
         OLED.writeStringNewLine("It's to cold for me please turn on the heating")
-    } else if (Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P15) > 25) {
+    } else if (Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P7) > 25) {
         OLED.writeStringNewLine("It's to hot for me please turn on down the heat")
     } else {
         OLED.writeStringNewLine("It's a good temperature for me :)")
@@ -55,16 +55,23 @@ function Rubbish () {
         . # # # .
         . # # # .
         `)
-    if (Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P15) < 15) {
+    if (Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_mm, DigitalPin.P15) > 15) {
         Environment.ledBrightness(AnalogPin.P3, true)
+        Environment.ledBrightness(AnalogPin.P2, false)
+        Environment.ledBrightness(AnalogPin.P1, false)
         OLED.writeStringNewLine("The rubbish bin is chill")
-    } else if (Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P15) < 30 && Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P15) > 16) {
+    } else if (Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_mm, DigitalPin.P15) < 6 && Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_mm, DigitalPin.P15) > 14) {
         Environment.ledBrightness(AnalogPin.P2, true)
+        Environment.ledBrightness(AnalogPin.P3, false)
+        Environment.ledBrightness(AnalogPin.P1, false)
         OLED.writeStringNewLine("The rubbish is slowly building")
     } else {
+        Environment.ledBrightness(AnalogPin.P2, false)
+        Environment.ledBrightness(AnalogPin.P3, false)
         Environment.ledBrightness(AnalogPin.P1, true)
         OLED.writeStringNewLine("Time to take out the rubbish")
     }
+    basic.pause(500)
 }
 OLED.init(128, 64)
 Clear_Screens()
