11e82e3de87d2a20330738e913bf092a20f1c1f7
Chianne <chiannelyfordshields@gmail.com>
2024-10-30 21:23:27 +1300

    Created a clear screens function and created a on start routine

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+function Clear_Screens () {
+    basic.clearScreen()
+    OLED.clear()
+}
+OLED.init(128, 64)
+Clear_Screens()
+basic.showIcon(IconNames.House)
+OLED.writeStringNewLine("System Starting")
 basic.forever(function () {
 	
 })
